104663de6279af3e0129f952c15f768aeea598b4
root <root@dev.regfire.net>
2015-07-07 16:36:36 +0200

    Line ending problem in windows

--- dev/dev-entrypoint.sh
+++ dev/dev-entrypoint.sh
@@ -5,7 +5,6 @@ cd /app
 
 #Check if we have been given $SETTINGS_FILE
 if [[ $SETTINGS_FILE ]]; then
-    #yes, but does the file exist?
     if [ -f $SETTINGS_FILE ]; then
         echo "=> $SETTINGS_FILE found.. starting meteor with settings!"
         meteor --settings $SETTINGS_FILE --port $PORT
