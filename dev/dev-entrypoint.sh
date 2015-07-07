5082eaad435f6756f8dd2f5826eb10f49d146965
root <risul.islam@regfire.com>
2015-07-07 15:09:45 +0200

    Dev build working

--- dev/dev-entrypoint.sh
+++ dev/dev-entrypoint.sh
@@ -2,3 +2,15 @@
 set -e
 
 cd /app
+
+#Check if we have been given $SETTINGS_FILE
+if [[ $SETTINGS_FILE ]]; then
+    #yes, but does the file exist?
+    if [ -f $SETTINGS_FILE ]; then
+        meteor --settings $SETTINGS_FILE --port $PORT
+    else
+        meteor --port $PORT
+    fi
+else
+    meteor --port $PORT
+fi
