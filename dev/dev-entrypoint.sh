557afa85732a739be1d51cb77b25a7359692becf
root <root@dev.regfire.net>
2015-07-07 16:42:02 +0200

    Line ending

--- dev/dev-entrypoint.sh
+++ dev/dev-entrypoint.sh
@@ -5,6 +5,7 @@ cd /app
 
 #Check if we have been given $SETTINGS_FILE
 if [[ $SETTINGS_FILE ]]; then
+    #yes, but does the file exist?
     if [ -f $SETTINGS_FILE ]; then
         echo "=> $SETTINGS_FILE found.. starting meteor with settings!"
         meteor --settings $SETTINGS_FILE --port $PORT
